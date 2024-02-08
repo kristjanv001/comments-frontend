3899138b6efb21620b1a96dafad132830630c315
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-02-08 08:34:37 +0200

    reusable card component

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Comment } from '../../interfaces/comment';
 import { User } from '../../interfaces/user';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './comment.component.html'
 })
 export class CommentComponent {
